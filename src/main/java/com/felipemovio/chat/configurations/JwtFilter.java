a7c772f925ebac4585c522921a753b0903d22a39
FelipeMovio <felipelsmovio@gmail.com>
2025-11-29 17:35:30 -0300

    create security config

--- src/main/java/com/felipemovio/chat/configurations/JwtFilter.java
+++ src/main/java/com/felipemovio/chat/configurations/JwtFilter.java
@@ -20,7 +20,8 @@ public class JwtFilter  extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String path = request.getRequestURI();
-        if (path.equals("/auth/gerarToken")
+        if (path.equals("/api/auth/login")
+                || path.startsWith("/api/auth/register")
                 || path.startsWith("/swagger-ui")
                 || path.startsWith("/v2/api-docs")
                 || path.startsWith("/v3/api-docs")
