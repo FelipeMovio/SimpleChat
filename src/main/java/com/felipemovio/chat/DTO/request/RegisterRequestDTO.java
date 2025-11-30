77df0b24ed1d5a6ce6f7558b5e5d29447b3d10e4
FelipeMovio <felipelsmovio@gmail.com>
2025-11-29 21:08:04 -0300

    update

--- src/main/java/com/felipemovio/chat/DTO/request/RegisterRequestDTO.java
+++ src/main/java/com/felipemovio/chat/DTO/request/RegisterRequestDTO.java
@@ -2,20 +2,22 @@ package com.felipemovio.chat.DTO.request;
 
 
 import com.felipemovio.chat.model.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RegisterRequestDTO {
-
+    @NotBlank
     private String nome;
 
-
+    @NotNull
     private Integer idade;
 
-
+    @NotBlank
     private String email;
 
-
+    @NotBlank
     private String senha;
 
     private Role role;
