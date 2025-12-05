e662ebd6fdefc17da434529e5decdaafd9546cc1
FelipeMovio <felipelsmovio@gmail.com>
2025-12-05 20:41:02 -0300

    override configureMessageBroker

--- src/main/java/com/felipemovio/chat/config/WebSocketConfig.java
+++ src/main/java/com/felipemovio/chat/config/WebSocketConfig.java
@@ -1,8 +1,14 @@
 package com.felipemovio.chat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+    }
 }
