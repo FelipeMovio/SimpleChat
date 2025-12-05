50e2cc9ae3b2c9a31d5d1578d0a1a500a8aa160a
FelipeMovio <felipelsmovio@gmail.com>
2025-12-05 20:41:42 -0300

    override registerStompEndpoints

--- src/main/java/com/felipemovio/chat/config/WebSocketConfig.java
+++ src/main/java/com/felipemovio/chat/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.felipemovio.chat.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
@@ -10,5 +11,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
     }
 }
