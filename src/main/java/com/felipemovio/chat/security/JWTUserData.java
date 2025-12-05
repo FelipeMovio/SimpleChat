d3d33aa38c4352033e2669bb6d057943dfe48e78
FelipeMovio <felipelsmovio@gmail.com>
2025-12-05 20:37:44 -0300

    new dependeyas

--- src/main/java/com/felipemovio/chat/security/JWTUserData.java
+++
@@ -1,9 +0,0 @@
-package com.felipemovio.chat.security;
-
-import lombok.Builder;
-
-import java.util.List;
-
-@Builder
-public record JWTUserData(Long userId, String email, List<String> roles ) {
-}
