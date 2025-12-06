09ab48fdcb7c8fc75fb425a73a2d9a703e46ee78
FelipeMovio <felipelsmovio@gmail.com>
2025-12-06 13:34:29 -0300

    @MessageMapping(/new-message)

--- src/main/java/com/felipemovio/chat/controller/ChatController.java
+++ src/main/java/com/felipemovio/chat/controller/ChatController.java
@@ -2,11 +2,13 @@ package com.felipemovio.chat.controller;
 
 import com.felipemovio.chat.DTOs.ChatInputDTO;
 import com.felipemovio.chat.DTOs.ChatOutputDTO;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
+    @MessageMapping("/new-message")
     public ChatOutputDTO message(ChatInputDTO input){
 
     }
