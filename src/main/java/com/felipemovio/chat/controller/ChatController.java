2518a8557845488d0b5131628b0d275610a6e9ec
FelipeMovio <felipelsmovio@gmail.com>
2025-12-06 13:36:06 -0300

    @SendTo(/topics/livechat)

--- src/main/java/com/felipemovio/chat/controller/ChatController.java
+++ src/main/java/com/felipemovio/chat/controller/ChatController.java
@@ -3,12 +3,14 @@ package com.felipemovio.chat.controller;
 import com.felipemovio.chat.DTOs.ChatInputDTO;
 import com.felipemovio.chat.DTOs.ChatOutputDTO;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
     @MessageMapping("/new-message")
+    @SendTo("/topics/livechat")
     public ChatOutputDTO message(ChatInputDTO input){
 
     }
